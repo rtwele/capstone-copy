8e02319a947e6263ec429db002732698fed4ce35
rtwele <ryantwele@gmail.com>
2020-12-03 11:52:56 -0500

    new repo

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static('client/build'));
+const uri = process.env.ATLAS_URI;
+        
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+  });
+
+const attractionsRouter = require('./routes/attractions')
+const contactRouter = require('./routes/contact')
+
+
+app.use('/attractions', attractionsRouter)
+app.use('/contact', contactRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ src/components/Videoplayer.js
@@ -0,0 +1,40 @@
+// <<<<<<< HEAD
+// // import React from 'react';
+// // import { Player } from 'video-react';
+// // import "../../node_modules/video-react/dist/video-react.css"; // import css
+// // <link rel="stylesheet" href="/css/video-react.css" />
+
+// <<<<<<< HEAD
+// // export default props => {
+// //   return (
+// //     <Player
+// //       playsInline
+// //       poster="/assets/poster.png"
+// //       src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+// //     />
+// //   );
+// // };
+// =======
+// =======
+// import React from 'react';
+// import { Player } from 'video-react';
+// import "../../node_modules/video-react/dist/video-react.css"; // import css
+// <link rel="stylesheet" href="/css/video-react.css" />
+
+// >>>>>>> 60e54bba71a7068cdf0f768190fbf9df442b3b0b
+// const Videoplayer = () => {
+//   return (
+//     <Player
+//       playsInline
+//       poster="/assets/poster.png"
+//       src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+//     />
+//   )
+// }
+
+// <<<<<<< HEAD
+// export default Videoplayer
+// >>>>>>> d1866783f35b617ceaf6dafef3858fd9837d1571
+// =======
+// export default Videoplayer
+// >>>>>>> 60e54bba71a7068cdf0f768190fbf9df442b3b0b
