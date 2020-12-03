0540dd67ed2f56a5185e7f1bd75b6780cdca3842
shudson327 <72040252+shudson327@users.noreply.github.com>
2020-12-03 10:45:55 -0500

    lets go

---
+++ src/components/Videoplayer.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+// import { Player } from 'video-react';
+// import "../../node_modules/video-react/dist/video-react.css"; // import css
+// <link rel="stylesheet" href="/css/video-react.css" />
+
+<<<<<<< HEAD
+// export default props => {
+//   return (
+//     <Player
+//       playsInline
+//       poster="/assets/poster.png"
+//       src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+//     />
+//   );
+// };
+=======
+const Videoplayer = () => {
+  return (
+    <Player
+      playsInline
+      poster="/assets/poster.png"
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    />
+  )
+}
+
+export default Videoplayer
+>>>>>>> d1866783f35b617ceaf6dafef3858fd9837d1571
