47df6e6bfcaf48aed555339ff449985221175d1b
Cameron Parker <cameronleeparker01@gmail.com>
2020-12-04 02:11:41 -0500

    aboutupdate

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Footer from '../components/Footer'
+import FooterHome from '../components/FooterHome'
 import '../styles/contact.css'
 
 export default class contact extends Component {
@@ -106,7 +106,7 @@ export default class contact extends Component {
             </div>
           </div>
       </div>
-      <Footer />
+      <FooterHome />
       </div>
     )
   }
