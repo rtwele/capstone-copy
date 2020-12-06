747a7a9bee1cbee0d01a67bb5ff756e34e805f2f
Jacob Huff <jacobhuff1313@gmail.com>
2020-12-06 17:30:56 -0500

    fixing backend for heroku

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, '../build')));
 const uri = process.env.ATLAS_URI;
         
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
@@ -31,9 +31,9 @@ app.use('/contact', contactRouter)
 
 /* Production */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('../build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../build'))
   })
 }
 
