95cc1512a38782881227389df03254909a273292
Jacob Huff <jacobhuff1313@gmail.com>
2020-12-06 17:53:57 -0500

    swapped file structure

--- server.js
+++ server.js
@@ -10,30 +10,28 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('client/build'))
 
-app.use(express.static(path.join(__dirname, '../build')));
 const uri = process.env.ATLAS_URI;
         
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
 );
 const connection = mongoose.connection;
-
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
-  });
+});
 
 const attractionsRouter = require('./routes/attractions')
 const contactRouter = require('./routes/contact')
-
 
 app.use('/attractions', attractionsRouter)
 app.use('/contact', contactRouter)
 
 /* Production */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../build'))
+  app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build'))
+    res.sendFile(path.resolve(__dirname, 'client/build'))
   })
 }
 
