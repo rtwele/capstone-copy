0f3df42dc2528f3ef3e8a4f97c1cd20a7ed6a26a
Cameron Parker <cameronleeparker01@gmail.com>
2020-12-06 03:27:31 -0500

    about

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,29 +20,29 @@ const About = () => {
         <div class="row">
           <div class="col-lg-4">
             <img src={Jheadshot} alt="Generic placeholder " width="140" height="140"></img>
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            <p><a class="btn btn-lg btn-primary" href=" " role="button">Browse gallery</a></p>
+            <h2>Jacob Huff</h2>
+            <p>Leader and brains of the group</p>
+            <p><a class="btn btn-lg btn-primary" href="https://www.linkedin.com/in/jacobhuff1/" role="button">View my Profile</a></p>
           </div>
           <div class="col-lg-4">
             <img src={Sheadshot} alt="Generic placeholder " width="140" height="140"></img>
-            <h2>Heading</h2>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            <p><a class="btn btn-lg btn-primary" href=" " role="button">Browse gallery</a></p>
+            <h2>Sierra Hudson</h2>
+            <p> The real leader of the group/Big boss </p>
+            <p><a class="btn btn-lg btn-primary" href="https://www.linkedin.com/in/sierra-hudson-75b8231b8/" role="button">View my profile</a></p>
           </div>
         </div>
         <div class="row">
           <div class="col-lg-4">
             <img src={Rheadshot} alt="Generic placeholder " width="140" height="140"></img>
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-lg btn-primary" href=" " role="button">Browse gallery</a></p>
+            <h2>Ryan Twele</h2>
+            <p>Surrogate father figure of the group.</p>
+            <p><a class="btn btn-lg btn-primary" href="https://www.linkedin.com/in/ryan-twele-a168181b8/" role="button">View my profile</a></p>
           </div>
           <div class="col-lg-4">
             <img src={CHeadshot} alt="Generic placeholder " width="140" height="140"></img>
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-lg btn-primary" href=" " role="button">Browse gallery</a></p>
+            <h2>Cameron Parker</h2>
+            <p> </p>
+            <p><a class="btn btn-lg btn-primary" href="https://www.linkedin.com/in/cameron-parker-47617b101/" role="button">View my profile</a></p>
           </div>
         </div>
       </div>
@@ -51,7 +51,7 @@ const About = () => {
 
       <div class="row featurette">
         <div class="col-md-7">
-          <h2 class="featurette-heading">Hard working crew <span class='sub'>It'll blow your mind.</span></h2>
+          <h2 class="featurette-heading">Hard working team <span class='sub'>It'll blow your mind.</span></h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
         <div class="col-md-5">
