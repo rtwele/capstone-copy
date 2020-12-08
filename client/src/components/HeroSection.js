a914277b3f24a58aeb58230d6112db3309553fc9
shudson327 <72040252+shudson327@users.noreply.github.com>
2020-12-08 16:11:27 -0500

    tuesdays

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { Button } from '../components/Button';
 import '../styles/HeroSection.css';
 import Video from '../videos/ohio.mp4';
-import {Button} from '../styles/Button.css';
 
 export const HeroSection = () => {
   return (
@@ -14,17 +14,18 @@ export const HeroSection = () => {
           buttonStyle='btn--test'
           buttonSize='btn--medium'
         >
-        Tours Here
+          Tours Here
         </Button>
       
       </div>
-     
+      {/* <iframe width="100%" height="1000" src="https://www.youtube.com/embed/G8EUiH00BrU" autoPlay loop muted frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
       <video autoPlay loop muted>
         <source src={Video} type='video/mp4' />
       </video>
+      
+    
     </div>
   );
 }
-
 
 
