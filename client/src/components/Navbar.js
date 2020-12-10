5c3d5991d10fafe45642de83a23a93f281fbeb99
Jacob Huff <jacobhuff1313@gmail.com>
2020-12-10 10:51:03 -0500

    dropdown navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom'
-
+import { NavLink } from 'react-router-dom'
+import $ from 'jquery'
 
 
 
 const Navbar = () => {
+    const handleClick = e => {
+      e.preventDefault();
+      document.getElementsByClassName('navTrigger')[0].classList.toggle('active');
+      document.getElementById('mainListDiv').classList.toggle('show_list');
+      $('#mainListDiv').fadeIn();
+    }
+
     return (
         <header>
         <nav className="nav">
             <div className="container">
                 <div className="logo">
-                    <a href="/">Home</a>
+                    <NavLink to="/">Home</NavLink>
                 </div>
                 <div id="mainListDiv" className="main_list">
                     <ul className="navlinks">
-                    <li><Link to="/about">About</Link></li>
+                    <li><NavLink to="/about">About</NavLink></li>
                     <li><NavLink to="/contact">Contact</NavLink></li>
                     <li><NavLink to="/addattraction">Add Attraction</NavLink></li>
                     <li><NavLink to="/viewattractions">View Attractions</NavLink></li>
                     </ul>
                 </div>
-                <span className="navTrigger">
+                <span onClick={handleClick} className="navTrigger">
                     <i></i>
                     <i></i>
                     <i></i>
